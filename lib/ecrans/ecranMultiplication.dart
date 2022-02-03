cd9f4f38f84e243c9ca0dc79629f0df8974df822
dhodin <dhodin@yahoo.com>
2022-02-03 23:44:35 +0100

    V 0.1

--- lib/ecrans/ecranMultiplication.dart
+++ lib/ecrans/ecranMultiplication.dart
@@ -14,6 +14,10 @@ class _EcranAccueilState extends State<EcranAccueil> {
   List<int> valeursMult = maMultiplication.Operation();
   double _tableChoisie = 5;
   String _resultat = "";
+  Color _couleurResultat = Colors.black;
+  int _nombreJuste = 0;
+  int _nombreQuestions = 0;
+  int _note = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +29,13 @@ class _EcranAccueilState extends State<EcranAccueil> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            child: Text(
-              "Table de " + _tableChoisie.toInt().toString(),
-              style: TextStyle(
-                fontSize: 30.0,
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 10.0, 10.0),
+            child: Center(
+              child: Text(
+                "Table de " + _tableChoisie.toInt().toString(),
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
               ),
             ),
           ),
@@ -73,22 +79,32 @@ class _EcranAccueilState extends State<EcranAccueil> {
           Padding(
             padding: EdgeInsets.all(16.0),
             child: OutlinedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orangeAccent),
+              ),
               onPressed: () {
                 setState(() {
-                  if ((valeursMult[0] * valeursMult[1]) == valeursMult[2])
+                  if ((valeursMult[0] * valeursMult[1]) == valeursMult[2]) {
                     _resultat = "Bravo !!! " +
                         valeursMult[0].toString() +
                         " x " +
                         valeursMult[1].toString() +
                         " = " +
                         valeursMult[2].toString();
-                  else
+                    _couleurResultat = Colors.green;
+                    _nombreJuste++;
+                  } else {
                     _resultat = "En non,  " +
                         valeursMult[0].toString() +
                         " x " +
                         valeursMult[1].toString() +
                         " = " +
                         valeursMult[3].toString();
+                    _couleurResultat = Colors.red;
+                  }
+                  _nombreQuestions++;
+                  _note = ((_nombreJuste * 20) / _nombreQuestions).round();
                   valeursMult = maMultiplication.Operation();
                 });
               },
@@ -99,23 +115,32 @@ class _EcranAccueilState extends State<EcranAccueil> {
           Padding(
             padding: EdgeInsets.all(16.0),
             child: OutlinedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orangeAccent),
+              ),
               onPressed: () {
                 setState(() {
-                  if ((valeursMult[0] * valeursMult[1]) == valeursMult[3])
+                  if ((valeursMult[0] * valeursMult[1]) == valeursMult[3]) {
                     _resultat = "Bravo !!! " +
                         valeursMult[0].toString() +
                         " x " +
                         valeursMult[1].toString() +
                         " = " +
                         valeursMult[3].toString();
-                  else
+                    _couleurResultat = Colors.green;
+                    _nombreJuste++;
+                  } else {
                     _resultat = "En non,  " +
                         valeursMult[0].toString() +
                         " x " +
                         valeursMult[1].toString() +
                         " = " +
                         valeursMult[2].toString();
-
+                    _couleurResultat = Colors.red;
+                  }
+                  _nombreQuestions++;
+                  _note = ((_nombreJuste * 20) / _nombreQuestions).round();
                   valeursMult = maMultiplication.Operation();
                 });
               },
@@ -125,10 +150,34 @@ class _EcranAccueilState extends State<EcranAccueil> {
           ),
           Padding(
             padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            child: Text(
-              _resultat,
-              style: TextStyle(
-                fontSize: 30.0,
+            child: Center(
+              child: Text(
+                _resultat,
+                style: TextStyle(
+                  color: _couleurResultat,
+                  fontSize: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: Divider(
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: Center(
+              child: Text(
+                "Note : " +
+                    _note.toString() +
+                    " / 20 (" +
+                    _nombreQuestions.toString() +
+                    ")",
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Table de Multiplication',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const EcranAccueil(),
     );
